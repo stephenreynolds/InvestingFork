4d505b9383823ec29a1849bdf2fde67e0fd08196
stephenreynolds <git@stephenreynolds.dev>
2020-10-04 09:50:18 -0400

    Add button that inverts colors to simulate a dark mode.

---
+++ ClientApp/src/app/theme-selector/theme-selector.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+
+enum Theme {
+  Light,
+  Dark
+}
+
+@Component({
+  selector: 'app-theme-selector',
+  templateUrl: './theme-selector.component.html',
+  styleUrls: ['./theme-selector.component.scss']
+})
+export class ThemeSelectorComponent implements OnInit {
+
+  public faThemeIcon = faMoon;
+  public theme: Theme = Theme.Light;
+
+  public get themeText() {
+    switch (this.theme) {
+      case Theme.Light:
+        return "light";
+      case Theme.Dark:
+        return "dark";
+    }
+  }
+
+  public ngOnInit(): void {
+    const themeText = localStorage.getItem('theme');
+    if (themeText) {
+      const theme = themeText === 'light' ? Theme.Light : Theme.Dark;
+      this.setTheme(theme);
+    }
+  }
+
+  public setTheme(theme: Theme) {
+    if (theme === Theme.Light) {
+      document.documentElement.classList.remove('dark-mode');
+      document.querySelectorAll('.inverted').forEach(result => {
+        result.classList.remove('invert');
+      });
+      localStorage.setItem('theme', 'light');
+      this.faThemeIcon = faMoon;
+      this.theme = Theme.Light;
+    }
+    else {
+      document.documentElement.classList.add('dark-mode');
+      document.querySelectorAll('.inverted').forEach(result => {
+        result.classList.add('invert');
+      });
+      localStorage.setItem('theme', 'dark');
+      this.faThemeIcon = faSun;
+      this.theme = Theme.Dark;
+    }
+  }
+
+  public onThemeToggle() {
+    switch (this.theme) {
+      case Theme.Light:
+        this.setTheme(Theme.Dark);
+        break;
+      case Theme.Dark:
+        this.setTheme(Theme.Light);
+        break;
+    }
+  }
+}
